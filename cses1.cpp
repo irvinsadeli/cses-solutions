c9f9699cbe8ec8202c6059a1044fb77f89dfc4de
irvinsadeli <irvinsadeli1638@gmail.com>
2025-11-13 01:43:31 +0100

    intro_1

---
+++ cses1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    long long a;
+    std::cin >> a;
+
+    std::cout << a;
+    while (a != 1){
+        if(a % 2 == 0){
+            a = a/2;
+        } else{
+            a = (a*3)+1;
+        }
+        std::cout << " " << a;
+    }
+}
